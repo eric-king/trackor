8eb47601b6a622eca6dbaba4d7e4b609d845729b
Eric King <eric@virtualkings.com>
2022-04-15 16:58:03 -0700

    Add beginning of ActivityLog functionality

---
+++ Features/Activity/Model/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace Trackor.Features.Activity.Model;
+
+public record Project
+{
+    public int Id { get; init; }
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public bool Active { get; init; }
+}
--- Features/ActivityLog/ActivityLogStore.cs
+++ Features/ActivityLog/ActivityLogStore.cs
@@ -1,13 +1,16 @@
 ﻿using Fluxor;
+using Trackor.Features.ActivityLog.Model;
 
 namespace Trackor.Features.ActivityLog;
 
 public record ActivityLogLoadAction();
+public record ActivityLogSetLogItemsAction(ActivityLogItem[] Items);
 public record ActivityLogSetLoadedAction(bool IsLoaded);
 
 public record ActivityLogState
 {
     public bool IsLoaded { get; init; }
+    public ActivityLogItem[] ActivityLogItems { get; init; }
 }
 
 public class ActivityLogFeature : Feature<ActivityLogState>
@@ -18,7 +21,8 @@ public class ActivityLogFeature : Feature<ActivityLogState>
     {
         return new ActivityLogState
         {
-            IsLoaded = false
+            IsLoaded = false,
+            ActivityLogItems = Array.Empty<ActivityLogItem>()
         };
     }
 }
@@ -33,6 +37,16 @@ public static class ActivityLogReducers
             IsLoaded = action.IsLoaded
         };
     }
+
+    [ReducerMethod]
+    public static ActivityLogState OnActivityLogSetLogItemsAction(ActivityLogState state, ActivityLogSetLogItemsAction action)
+    {
+        return state with
+        {
+            ActivityLogItems = action.Items,
+            IsLoaded = true
+        };
+    }
 }
 
 public class ActivityLogEffects 
@@ -42,6 +56,22 @@ public class ActivityLogEffects
     {
         // simulate loading something
         await Task.Delay(500);
-        dispatcher.Dispatch(new ActivityLogSetLoadedAction(true));
+        var items = BuildSampleActivityLogItems();
+
+        dispatcher.Dispatch(new ActivityLogSetLogItemsAction(items));
+    }
+
+    private ActivityLogItem[] BuildSampleActivityLogItems()
+    {
+        ActivityLogItem[] activityLogItems = new ActivityLogItem[]
+        {
+        new ActivityLogItem { Id = 1, CategoryId = null, ProjectId = null, Title = "Test Item 1", Narrative = "This is test item 1", Date = DateOnly.FromDateTime(DateTime.Now), Duration = TimeSpan.FromMinutes(15) },
+        new ActivityLogItem { Id = 2, CategoryId = null, ProjectId = null, Title = "Test Item 2", Narrative = "This is test item 2", Date = DateOnly.FromDateTime(DateTime.Now), Duration = TimeSpan.FromMinutes(20) },
+        new ActivityLogItem { Id = 3, CategoryId = null, ProjectId = null, Title = "Test Item 3", Narrative = "This is test item 3", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)), Duration = TimeSpan.FromMinutes(35) },
+        new ActivityLogItem { Id = 4, CategoryId = null, ProjectId = null, Title = "Test Item 4", Narrative = "This is test item 4", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)), Duration = TimeSpan.FromMinutes(22) },
+        new ActivityLogItem { Id = 5, CategoryId = null, ProjectId = null, Title = "Test Item 5", Narrative = "This is test item 5", Date = DateOnly.FromDateTime(DateTime.Now.AddDays(-2)), Duration = TimeSpan.FromMinutes(9) }
+        };
+
+        return activityLogItems;
     }
 }
