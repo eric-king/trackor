a2f91dcbea16feddc1beec665be29cd05a45d39a
Eric King <eric@virtualkings.com>
2022-04-23 17:01:11 -0700

    Added ability to download the sqlite database from browser cache

--- Features/Core/CoreStore.cs
+++ Features/Core/CoreStore.cs
@@ -1,4 +1,5 @@
 ﻿using Fluxor;
+using Microsoft.JSInterop;
 using SqliteWasmHelper;
 using Trackor.Database;
 using Trackor.Features.Core.Model;
@@ -6,18 +7,23 @@ using Trackor.Features.Core.Model;
 namespace Trackor.Features.Core;
 
 public record CoreLoadCategoriesAction();
-public record CoreLoadProjectsAction();
 public record CoreSetCategoriesAction(Category[] Categories);
-public record CoreSetProjectsAction(Project[] Projects);
 public record CoreCategorySaveAction(Category Category);
 public record CoreCategoryAddAction(Category Category);
+
+public record CoreLoadProjectsAction();
+public record CoreSetProjectsAction(Project[] Projects);
 public record CoreProjectSaveAction(Project Project);
 public record CoreProjectAddAction(Project Project);
+
+public record CoreBuildDbDownloadUrlAction();
+public record CoreSetDbDownloadUrlAction(string Url);
 
 public record CoreState
 {
     public Category[] Categories { get; init; }
     public Project[] Projects { get; init; }
+    public string DbDownloadUrl { get; init; }
 }
 
 public class CoreFeature : Feature<CoreState>
@@ -29,15 +35,16 @@ public class CoreFeature : Feature<CoreState>
         return new CoreState
         {
             Categories = Array.Empty<Category>(),
-            Projects = Array.Empty<Project>()
+            Projects = Array.Empty<Project>(),
+            DbDownloadUrl = string.Empty
         };
     }
 }
 
-public static class CoreReducers 
+public static class CoreReducers
 {
     [ReducerMethod]
-    public static CoreState CoreSetCategories(CoreState state, CoreSetCategoriesAction action)
+    public static CoreState OnSetCategories(CoreState state, CoreSetCategoriesAction action)
     {
         return state with
         {
@@ -58,7 +65,7 @@ public static class CoreReducers
     }
 
     [ReducerMethod]
-    public static CoreState CoreSetProducts(CoreState state, CoreSetProjectsAction action)
+    public static CoreState OnSetProducts(CoreState state, CoreSetProjectsAction action)
     {
         return state with
         {
@@ -77,48 +84,76 @@ public static class CoreReducers
             Projects = projectList.OrderBy(x => x.Title).ToArray()
         };
     }
+
+    [ReducerMethod]
+    public static CoreState OnSetDbDownloadUrl(CoreState state, CoreSetDbDownloadUrlAction action)
+    {
+        return state with
+        {
+            DbDownloadUrl = action.Url
+        };
+    }
 }
 
-public class CoreEffects 
+public class CoreEffects
 {
-    private readonly ISqliteWasmDbContextFactory<TrackorContext> _dbFactory;
+    private readonly ISqliteWasmDbContextFactory<TrackorContext> _db;
+    private readonly IJSRuntime _js;
+    private readonly IState<CoreState> _state;
 
-    public CoreEffects(ISqliteWasmDbContextFactory<TrackorContext> dbFactory)
+    public CoreEffects(ISqliteWasmDbContextFactory<TrackorContext> dbFactory, IJSRuntime jsRuntime, IState<CoreState> state)
     {
-        _dbFactory = dbFactory;
+        _db = dbFactory;
+        _js = jsRuntime;
+        _state = state;
     }
 
     [EffectMethod(typeof(CoreLoadCategoriesAction))]
     public async Task OnLoadCategories(IDispatcher dispatcher)
     {
-        using var dbContext = await _dbFactory.CreateDbContextAsync();
+        using var dbContext = await _db.CreateDbContextAsync();
         var items = dbContext.Categories.ToArray();
         dispatcher.Dispatch(new CoreSetCategoriesAction(items));
-    }
-
-    [EffectMethod(typeof(CoreLoadProjectsAction))]
-    public async Task OnLoadProjects(IDispatcher dispatcher)
-    {
-        using var dbContext = await _dbFactory.CreateDbContextAsync();
-        var items = dbContext.Projects.ToArray();
-        dispatcher.Dispatch(new CoreSetProjectsAction(items));
     }
 
     [EffectMethod]
     public async Task OnCategorySave(CoreCategorySaveAction action, IDispatcher dispatcher)
     {
-        using var dbContext = await _dbFactory.CreateDbContextAsync();
+        using var dbContext = await _db.CreateDbContextAsync();
         dbContext.Categories.Add(action.Category);
         await dbContext.SaveChangesAsync();
         dispatcher.Dispatch(new CoreCategoryAddAction(action.Category));
     }
 
+    [EffectMethod(typeof(CoreLoadProjectsAction))]
+    public async Task OnLoadProjects(IDispatcher dispatcher)
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        var items = dbContext.Projects.ToArray();
+        dispatcher.Dispatch(new CoreSetProjectsAction(items));
+    }
+
     [EffectMethod]
     public async Task OnProjectSave(CoreProjectSaveAction action, IDispatcher dispatcher)
     {
-        using var dbContext = await _dbFactory.CreateDbContextAsync();
+        using var dbContext = await _db.CreateDbContextAsync();
         dbContext.Projects.Add(action.Project);
         await dbContext.SaveChangesAsync();
         dispatcher.Dispatch(new CoreProjectAddAction(action.Project));
+    }
+
+    [EffectMethod(typeof(CoreBuildDbDownloadUrlAction))]
+    public async Task OnBuildDbDownloadUrl(IDispatcher dispatcher)
+    {
+        // bail if the download url has already been generated
+        if (!string.IsNullOrEmpty(_state.Value.DbDownloadUrl)) return;
+
+        var jsModule = await _js.InvokeAsync<IJSObjectReference>("import", "./dbDownload.js");
+        var downloadUrl = await jsModule.InvokeAsync<string>("generateDownloadUrl");
+
+        if (!string.IsNullOrEmpty(downloadUrl))
+        {
+            dispatcher.Dispatch(new CoreSetDbDownloadUrlAction(downloadUrl));
+        }
     }
 }
---
+++ wwwroot/dbDownload.js
@@ -0,0 +1,18 @@
+﻿export async function generateDownloadUrl() {
+
+    window.sqlitedb = window.sqlitedb || {
+        init: false,
+        cache: await caches.open('SqliteWasmHelper')
+    };
+
+    const resp = await window.sqlitedb.cache.match('/data/cache/trackor.db');
+
+    if (resp && resp.ok) {
+        const dbBlob = await resp.blob();
+        if (dbBlob) {
+            return URL.createObjectURL(dbBlob);
+        }
+    }
+
+    return '';
+}
