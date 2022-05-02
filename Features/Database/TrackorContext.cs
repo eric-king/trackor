95aa9c5730b46f76e52d3eb5dead9bebf5154153
Eric King <eric@virtualkings.com>
2022-05-02 15:18:08 -0700

    Removed Root Descriptor file

--- Features/Database/TrackorContext.cs
+++ Features/Database/TrackorContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 using Trackor.Features.ActivityLog;
 using Trackor.Features.Categories;
 using Trackor.Features.Projects;
@@ -7,10 +8,17 @@ namespace Trackor.Features.Database
 {
     public class TrackorContext : DbContext
     {
+        /// <summary>
+        /// FIXME: This is required for EF Core 6.0 as it is not compatible with trimming.
+        /// </summary>
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+        private static Type _keepDateOnly = typeof(DateOnly);
+
         public TrackorContext(DbContextOptions<TrackorContext> options) : base(options) { }
 
         public DbSet<ActivityLogItem> ActivityLogItems { get; set; } = null;
         public DbSet<Category> Categories { get; set; } = null;
         public DbSet<Project> Projects { get; set; } = null;
     }
+
 }
