79d735cead9bd2fa84c167b2e24ed82ec63172a0
Eric King <eric@virtualkings.com>
2022-05-04 22:09:42 -0700

    Set some autofocus and enter-key handling

--- Features/Projects/Project.cs
+++ Features/Projects/Project.cs
@@ -4,6 +4,5 @@ public record Project
 {
     public int Id { get; init; }
     public string Title { get; init; }
-    public string Description { get; init; }
     public bool Active { get; init; }
 }
