6962ab091ac725499fc9ddd8806d5d87579b4f96
Eric King <eric@virtualkings.com>
2022-05-19 13:26:30 -0700

    Added some Snackbar Effects and show a message when Activities are copied to the clipboard and when the Pomodoro timer finishes.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Fluxor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor;
 using MudBlazor.Services;
 using SqliteWasmHelper;
 using Trackor;
@@ -14,8 +15,18 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new PomodoroTimerService());
-builder.Services.AddMudServices();
 builder.Services.AddFluxor(o => o.ScanAssemblies(typeof(Program).Assembly));
 builder.Services.AddSqliteWasmDbContextFactory<TrackorContext>(options => options.UseSqlite("Data Source=trackor.sqlite3"));
-
+builder.Services.AddMudServices(config => 
+{
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomEnd;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+    config.SnackbarConfiguration.MaxDisplayedSnackbars = 10;
+});
 await builder.Build().RunAsync();
