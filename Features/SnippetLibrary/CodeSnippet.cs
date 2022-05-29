328dcb568910c6bcdf1dcb3bca0a26c90ed9771a
Eric King <eric@virtualkings.com>
2022-05-29 11:35:57 -0700

    Add a SourceUrl property to the CodeSnippet

--- Features/SnippetLibrary/CodeSnippet.cs
+++ Features/SnippetLibrary/CodeSnippet.cs
@@ -1,4 +1,6 @@
-﻿namespace Trackor.Features.SnippetLibrary;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trackor.Features.SnippetLibrary;
 
 public class CodeSnippet
 {
@@ -6,4 +8,17 @@ public class CodeSnippet
     public string Label { get; set; }
     public string Content { get; set; }
     public string Language { get; set; }
+    public string SourceUrl { get; set; }
+
+    public List<ValidationException> Validate() 
+    {
+        var exceptions = new List<ValidationException>();
+
+        if (new UrlAttribute().IsValid(SourceUrl) == false) 
+        {
+            exceptions.Add(new ValidationException($"SourceUrl is not a valid URL: {SourceUrl}"));
+        }
+
+        return exceptions;
+    }
 }
