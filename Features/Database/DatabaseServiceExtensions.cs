2278546100048a7037be615d7b6245e9eb547034
Eric King <eric@virtualkings.com>
2022-06-13 17:28:38 -0700

    Added some basic statistics to the Database page

--- Features/Database/DatabaseServiceExtensions.cs
+++ Features/Database/DatabaseServiceExtensions.cs
@@ -11,6 +11,7 @@ public static class DatabaseServiceExtensions
         services.AddSingleton<ApplicationSettingRepository>();
         services.AddSingleton<CategoryRepository>();
         services.AddSingleton<CodeSnippetRepository>();
+        services.AddSingleton<DatabaseStatsRepository>();
         services.AddSingleton<LinkLibraryRepository>();
         services.AddSingleton<ProjectRepository>();
         services.AddSingleton<TaskListRepository>();
---
+++ Features/Database/Repositories/DatabaseStatsRepository.cs
@@ -0,0 +1,49 @@
+﻿using SqliteWasmHelper;
+
+namespace Trackor.Features.Database.Repositories;
+
+public class DatabaseStatsRepository
+{
+    private readonly ISqliteWasmDbContextFactory<TrackorContext> _db;
+
+    public DatabaseStatsRepository(ISqliteWasmDbContextFactory<TrackorContext> db)
+    {
+        _db = db;
+    }
+
+    public async Task<int> GetActivityLogItemCount() 
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.ActivityLogItems.Count();
+    }
+
+    public async Task<int> GetCategoryCount()
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.Categories.Count();
+    }
+
+    public async Task<int> GetCodeSnippetCount()
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.CodeSnippets.Count();
+    }
+
+    public async Task<int> GetLinkCount()
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.Links.Count();
+    }
+
+    public async Task<int> GetProjectCount()
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.Projects.Count();
+    }
+
+    public async Task<int> GetTaskCount()
+    {
+        using var dbContext = await _db.CreateDbContextAsync();
+        return dbContext.TaskListItems.Count();
+    }
+}
